753127ffb1fea2697b35fb99c919b1bd9ae97766
ErikGaston <erikmartinez_cba@hotmail.com>
2020-08-30 22:01:26 -0300

    Preload, Cards

--- MSoluciones/js/app.js
+++ MSoluciones/js/app.js
@@ -1,9 +1,17 @@
-﻿$(window).scroll(function () {
+﻿$(window).on('load', function () {
+    $(".preloader").delay(350).fadeOut('slow');
+    $("body").delay(350).css({ 'overflow': "visible" });    
+});
+
+$(window).scroll(function () {
     if ($(".navbar").offset().top > 50) {
         $(".navbar").css('background-color', '#3B3D3C');
+
+
       }
     else {
         $(".navbar").css('background-color', '');
+
     }
 
 });
